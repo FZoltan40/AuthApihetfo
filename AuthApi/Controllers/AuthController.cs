7bf74f7a9e45bfdf33902e63320866c835f48f22
FZoltan40 <farkasz@kkszki.hu>
2025-01-13 09:35:44 +0100

    Auth start template is created.

---
+++ AuthApi/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using AuthApi.Models.Dtos;
+using AuthApi.Services.IService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthApi.Controllers
+{
+    [Route("auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuth auth;
+
+        public AuthController(IAuth auth)
+        {
+            this.auth = auth;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> LoginPost(LoginRequestDto loginRequestDto)
+        {
+            return Ok();
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult> RegisterPost(RegisterRequestDto registerRequestDto)
+        {
+            return Ok();
+        }
+
+    }
+}
---
+++ AuthApi/Services/IService/IAuth.cs
@@ -0,0 +1,10 @@
+﻿using AuthApi.Models.Dtos;
+
+namespace AuthApi.Services.IService
+{
+    public interface IAuth
+    {
+        Task<object> Login(LoginRequestDto loginRequestDto);
+        Task<object> Register(RegisterRequestDto registerRequestDto);
+    }
+}
