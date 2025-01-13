299b0f58267940072ded67808fc2956053bc32c6
FZoltan40 <farkasz@kkszki.hu>
2025-01-13 10:06:11 +0100

    JwtGenerator template is created.

---
+++ AuthApi/Services/TokenGenarator.cs
@@ -0,0 +1,13 @@
+﻿using AuthApi.Models;
+using AuthApi.Services.IService;
+
+namespace AuthApi.Services
+{
+    public class TokenGenarator : ITokenGenerator
+    {
+        public string GenerateToken(ApplicationUser applicationUser, IEnumerable<string> roles)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- AuthApi/obj/Debug/net8.0/AuthApi.AssemblyInfo.cs
+++ AuthApi/obj/Debug/net8.0/AuthApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bf74f7a9e45bfdf33902e63320866c835f48f22")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
